bd93081e50556209b3227e88b08b9003cddc0436
Yogesh Jadhav <yogse7en@gmail.com>
2018-07-06 01:08:00 +0530

    Added runner scripts

--- ml_services/settings.py
+++ ml_services/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mti&d$321z+)z6vaens3g^1j*0wuetqu+=vtg-fv!)+v(tgoi!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
---
+++ prod.sh
@@ -0,0 +1,1 @@
+#!/usr/bin/env bash# move to project directorycd /home/yogesh/ml-apps/ml-services# start virtualenvtensorflow# start the gunicorn process in nohup (background)nohup gunicorn ml_services.wsgi:application --bind 127.0.0.1:8000 &
