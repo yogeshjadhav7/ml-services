a1548f5b80138601494f0fc9b7dc28bdd2f625a4
Yogesh Jadhav <yogse7en@gmail.com>
2018-08-16 23:40:15 +0530

    Updated prod.sh

--- prod.sh
+++ prod.sh
@@ -3,9 +3,12 @@
 # move to project directory
 cd /home/yogesh/ml-apps/ml-services
 
-# start virtualenv
-tensorflow
+# kill the previously running instance of the service on port 8000
+lsof -n -i :8000 | grep LISTEN | awk '{ print $2 }' | uniq | xargs kill -9
 
-# start the gunicorn process in nohup (background)
+# delete the nohup.out file
+rm nohup.out
+
+# start the gunicorn process in background using nohup
 nohup gunicorn ml_services.wsgi:application --bind 127.0.0.1:8000 &
 
